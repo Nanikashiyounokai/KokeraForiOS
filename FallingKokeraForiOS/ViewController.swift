0c9125571d28729b0a26d04a96cd52970c55f81f
Yuk1-Fukuoka <fuku0cap@gmail.com>
2023-05-07 11:58:56 +0900

    Initial Commit

---
+++ FallingKokeraForiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FallingKokeraForiOS
+//
+//  Created by 福岡　佑季 on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
