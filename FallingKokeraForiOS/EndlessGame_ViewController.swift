e13602c81d81b1bf8c00e357e90912a3a6ad08f8
yamaguchi61169 <kyama61169@gmail.com>
2023-07-27 12:59:00 +0900

    戻るボタンの削除

--- FallingKokeraForiOS/EndlessGame_ViewController.swift
+++ FallingKokeraForiOS/EndlessGame_ViewController.swift
@@ -8,6 +8,8 @@ class EndlessGameController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
 
         let skView = SKView(frame: view.bounds)
         view.addSubview(skView)
